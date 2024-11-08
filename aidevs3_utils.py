667419c438d66067af8d7cdf1292e27b7871bb3d
dtrznadel <trznadel.dom@gmail.com>
2024-11-08 16:42:05 +0100

    day 5 modifications

--- aidevs3_utils.py
+++ aidevs3_utils.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 aidevs3_api_key = os.getenv("AIDEVS3_API_KEY")
+domain = os.getenv("AIDEVS3_DOMAIN")
 
 
 class AIDevs3Utils:
@@ -31,10 +32,13 @@ class AIDevs3Utils:
         question_element = soup.find(id=id)
         return question_element.text.strip()
 
-    def verify_results(task, answer, endpoint):
+    def verify_results(
+        task, answer, endpoint=f"https://centrala.{domain}/report"
+    ):
         payload = {"task": task, "apikey": aidevs3_api_key, "answer": answer}
-        response = requests.post(endpoint, json=payload)
-        response.raise_for_status()
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(endpoint, headers=headers, json=payload)
+        #        response.raise_for_status()
         return response.json()
 
 
